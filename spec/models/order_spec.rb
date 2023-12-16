d56967d3d24bb35516dc56f074695d02475f61f1
Huy Nhat Ngo <huyngo248@gmail.com>
2023-12-16 15:01:11 +0700

    update model annotation

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :bigint           not null, primary key
+#  user_id    :bigint           not null
+#  price      :decimal(, )
+#  quantity   :integer
+#  order_type :string           default("buy"), not null
+#  status     :string           default("sell"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
